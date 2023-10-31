30a4a1c416cee35783f37d7998ad515ac2743dd1
Mara Cunningham <mara.james.codes@gmail.com>
2023-10-31 14:17:54 -0500

    Having issues with blogsite rendering post list template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 # Create your views here.
 
 def post_list(request):
-    posts = Post.publishedall()
+    posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
